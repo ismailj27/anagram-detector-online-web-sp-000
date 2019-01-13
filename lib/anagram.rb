5fba95c80268a47bfc727abb2f2e6eb3c526c5c7
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-13 03:16:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,11 +9,11 @@ class Anagram
   end
   
   def match(words)
-    words.each do |each_word|
+    words.find do |each_word|
       if each_word.length == @word.length
       each_word = each_word.split("")
       each_word.each do |a_letter|
-        a_letter
+        @word.include?(a_letter)
       
     end
   end
