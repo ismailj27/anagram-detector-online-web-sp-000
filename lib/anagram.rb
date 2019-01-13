3276bc998af3f6d81c5e14cd58685ca722506f35
Ismail Jalloh <ijalloh21@gmail.com>
2019-01-13 03:34:44 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,8 @@ class Anagram
   end
   
   def match(words)
-    words.find do |d_word|
+    rtn_arr
+    words. do |d_word|
      (d_word.length == @word.length) && (d_word.split("").sort.join == @word.split("").sort.join)
     end
   end
